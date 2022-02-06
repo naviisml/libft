972e6522a480e9329f64dc8120ae6c9da001337f
Navi <mailvoornavi@gmail.com>
2022-02-06 13:44:28 +0100

    bugfix

--- srcs/ft_strcmpchr.c
+++ srcs/ft_strcmpchr.c
@@ -6,23 +6,23 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 04:14:45 by nismail       #+#    #+#                 */
-/*   Updated: 2022/01/30 01:28:03 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/06 13:43:57 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
  * The ft_strcmpchr() functions lexicographically compare 
  * the null-terminated string str with char c.
- * 
- * This is subject to change, and therefor not included in libft (yet)
  */
 int	ft_strcmpchr(const char *str, const char c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != c)
+	while (str[i] != '\0')
 	{
+		if (str[i] != c)
+			return (1);
 		i++;
 	}
 	return ((int)str[i] - c);
